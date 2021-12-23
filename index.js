454b3cc77f9f6061c9d1fcb323a2e11c5e009343
fahim-tonmoy <43146740+fahim-tonmoy@users.noreply.github.com>
2021-12-23 14:20:48 +0600

    third commit

--- index.js
+++ index.js
@@ -13,20 +13,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 // const serviceAccount = require('./doctorsportal-702b0-firebase-adminsdk-fjxms-8ab8fe7081.json');
-// const serviceAccount = {
-//   type: process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
-//   project_id: process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID ,
-//   private_key_id: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID ,
-//   private_key: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
-//   client_email: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL ,
-//   client_id: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID ,
-//   auth_uri: process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI ,
-//   token_uri: process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI ,
-//   auth_provider_x509_cert_url: process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL ,
-//   client_x509_cert_url: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL ,
-// };
+const serviceAccount = {
+  type: process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
+  project_id: process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID ,
+  private_key_id: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID ,
+  private_key: process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL ,
+  client_id: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID ,
+  auth_uri: process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI ,
+  token_uri: process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI ,
+  auth_provider_x509_cert_url: process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL ,
+  client_x509_cert_url: process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL ,
+};
 
 
 // console.log(serviceAccount);
